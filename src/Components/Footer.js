1b4e7f08386d53f0bac56508d7e426cb50b71b3a
moon-light-night <baskakov.11111@mail.ru>
2021-12-20 00:47:55 +0300

    init

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <p>Its footer</p>
+      <ul>
+        <li>link 1</li>
+        <li>link 2</li>
+        <li>link 3</li>
+    </ul>
+    </div>
+  );
+}
+
+export default Footer
